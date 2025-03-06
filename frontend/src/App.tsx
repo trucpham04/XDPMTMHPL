83f4b1386e03b5b48722e5ebe93cc9aad5e26b86
Truc Pham <trucpham04@proton.me>
2025-03-07 02:56:05 +0700

    Add routing with react-router-dom and create Home and About pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,8 @@
-import { Button } from "@/components/ui/button";
+import RoutesComponent from "@/routes/index";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <RoutesComponent />
   );
 }
 
