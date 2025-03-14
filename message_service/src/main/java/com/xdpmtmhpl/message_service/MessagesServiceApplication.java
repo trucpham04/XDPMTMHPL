345c4ec3a377dff7e01bb442cd1aeaa479dd888b
Truc Pham <trucpham04@proton.me>
2025-03-14 11:40:21 +0700

    feat: Initialize message service

---
+++ message_service/src/main/java/com/xdpmtmhpl/message_service/MessagesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xdpmtmhpl.message_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagesServiceApplication.class, args);
+	}
+
+}
