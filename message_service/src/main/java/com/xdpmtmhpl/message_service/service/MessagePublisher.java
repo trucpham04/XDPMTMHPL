87af0f855eda24a45da22e16ce64e2fe0c121eb8
Truc Pham <trucpham04@proton.me>
2025-04-22 15:55:05 +0700

    update: simple RabbitMQ settings

--- message_service/src/main/java/com/xdpmtmhpl/message_service/service/MessagePublisher.java
+++
@@ -1,9 +0,0 @@
-package com.xdpmtmhpl.message_service.service;
-
-public interface MessagePublisher {
-    <T> void publishToService(String targetService, String messageType, T payload);
-
-    <T> void publishToTopic(String topic, String messageType, T payload);
-
-    <T> void broadcastToAll(String messageType, T payload);
-}
