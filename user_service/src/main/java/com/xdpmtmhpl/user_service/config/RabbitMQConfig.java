a2333335458dd2baad80d60987910e15cdb9e4be
Melanie-Elain <baohan.tbh0406@gmail.com>
2025-04-23 22:50:58 +0700

    23/4

--- user_service/src/main/java/com/xdpmtmhpl/user_service/config/RabbitMQConfig.java
+++ user_service/src/main/java/com/xdpmtmhpl/user_service/config/RabbitMQConfig.java
@@ -1,75 +1,76 @@
-package com.xdpmtmhpl.user_service.config;
 
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// package com.xdpmtmhpl.user_service.config;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+// import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+// import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+// import org.springframework.amqp.rabbit.core.RabbitAdmin;
+// import org.springframework.amqp.rabbit.core.RabbitTemplate;
+// import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+// import org.springframework.amqp.support.converter.MessageConverter;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import org.springframework.amqp.core.Queue;
+// import com.fasterxml.jackson.databind.DeserializationFeature;
+// import com.fasterxml.jackson.databind.ObjectMapper;
+// import com.fasterxml.jackson.databind.SerializationFeature;
 
-@Configuration
-public class RabbitMQConfig {
+// import org.springframework.amqp.core.Queue;
 
-    @Value("${spring.rabbitmq.host}")
-    private String host;
+// @Configuration
+// public class RabbitMQConfig {
 
-    @Value("${spring.rabbitmq.port}")
-    private int port;
+//     @Value("${spring.rabbitmq.host}")
+//     private String host;
 
-    @Value("${spring.rabbitmq.username}")
-    private String username;
+//     @Value("${spring.rabbitmq.port}")
+//     private int port;
 
-    @Value("${spring.rabbitmq.password}")
-    private String password;
+//     @Value("${spring.rabbitmq.username}")
+//     private String username;
 
-    @Bean
-    public ConnectionFactory connectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-        connectionFactory.setHost(host);
-        connectionFactory.setPort(port);
-        connectionFactory.setUsername(username);
-        connectionFactory.setPassword(password);
-        return connectionFactory;
-    }
+//     @Value("${spring.rabbitmq.password}")
+//     private String password;
 
-    @Bean
-    public MessageConverter jsonMessageConverter() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+//     @Bean
+//     public ConnectionFactory connectionFactory() {
+//         CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+//         connectionFactory.setHost(host);
+//         connectionFactory.setPort(port);
+//         connectionFactory.setUsername(username);
+//         connectionFactory.setPassword(password);
+//         return connectionFactory;
+//     }
 
-        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter(mapper);
+//     @Bean
+//     public MessageConverter jsonMessageConverter() {
+//         ObjectMapper mapper = new ObjectMapper();
+//         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+//         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-        converter.setClassMapper(null);
-        return converter;
-    }
+//         Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter(mapper);
 
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(jsonMessageConverter());
-        return template;
-    }
+//         converter.setClassMapper(null);
+//         return converter;
+//     }
 
-    @Bean
-    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
-        return new RabbitAdmin(connectionFactory);
-    }
+//     @Bean
+//     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+//         RabbitTemplate template = new RabbitTemplate(connectionFactory);
+//         template.setMessageConverter(jsonMessageConverter());
+//         return template;
+//     }
 
-    @Value("${rabbitmq.queue.message}")
-    private String messageQueueName;
+//     @Bean
+//     public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+//         return new RabbitAdmin(connectionFactory);
+//     }
 
-    @Bean
-    public Queue messageQueue() {
-        return new Queue(messageQueueName, true);
-    }
-}
+//     @Value("${rabbitmq.queue.message}")
+//     private String messageQueueName;
+
+//     @Bean
+//     public Queue messageQueue() {
+//         return new Queue(messageQueueName, true);
+//     }
+// }
