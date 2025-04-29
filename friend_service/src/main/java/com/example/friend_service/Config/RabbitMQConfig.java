4593b4d27b5f6d9f1662db317ea4d4ef997325c8
hawng04 <165697180+hawng04@users.noreply.github.com>
2025-04-30 00:02:59 +0700

    Update RabbitMQConfig.java

--- friend_service/src/main/java/com/example/friend_service/Config/RabbitMQConfig.java
+++ friend_service/src/main/java/com/example/friend_service/Config/RabbitMQConfig.java
@@ -1,37 +1,61 @@
-// package main.java.com.example.friend_service.Config;
+package main.java.com.example.friend_service.Config;
 
-// import org.springframework.amqp.core.Queue;
-// import org.springframework.amqp.core.TopicExchange;
-// import org.springframework.amqp.core.Binding;
-// import org.springframework.amqp.core.BindingBuilder;
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import org.springframework.amqp.core.Queue;
 
-// @Configuration
-// public class RabbitMQConfig {
+@Configuration
+public class RabbitMQConfig {
 
-//     @Value("${rabbitmq.queue}")
-//     private String queue;
+    @Value("${spring.rabbitmq.host}")
+    private String host;
 
-//     @Value("${rabbitmq.exchange}")
-//     private String exchange;
+    @Value("${spring.rabbitmq.port}")
+    private int port;
 
-//     @Value("${rabbitmq.routing-key}")
-//     private String routingKey;
+    @Value("${spring.rabbitmq.username}")
+    private String username;
 
-//     @Bean
-//     public Queue queue() {
-//         return new Queue(queue, true);
-//     }
+    @Value("${spring.rabbitmq.password}")
+    private String password;
 
-//     @Bean
-//     public TopicExchange exchange() {
-//         return new TopicExchange(exchange);
-//     }
+    @Value("${rabbitmq.queue.friend}")
+    private String friendQueue;
 
-//     @Bean
-//     public Binding binding(Queue queue, TopicExchange exchange) {
-//         return BindingBuilder.bind(queue).to(exchange).with(routingKey);
-//     }
-// }
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory factory = new CachingConnectionFactory(host, port);
+        factory.setUsername(username);
+        factory.setPassword(password);
+        return factory;
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return new Jackson2JsonMessageConverter(mapper);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+
+    @Bean
+    public Queue friendQueue() {
+        return new Queue(friendQueue, true);
+    }
+}
